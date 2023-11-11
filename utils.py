81ca692d730fb33f680846c8669e15083554334e
indrish38 <indrishsundar@gmail.com>
2023-11-11 22:32:01 +0530

    update

--- utils.py
+++ utils.py
@@ -85,7 +85,7 @@ async def download_media(client, message):
         val = current * 30 // total
         txt = f'''Downloading...\n[{val*'█'}{(30-val)*'▒'}] {current*100/total:.2f}%
         File Name : {message.document.file_name}
-        Progress : {current/1024/1024:.2f} of {total/1024/1024:.2f} MB'''
+        Progress : {(current/1024)/1024:.2f} of {(total/1024)/1024:.2f} MB'''
         if(msg.text != txt):
             await msg.edit_text(txt)
             msg.text = txt
