f662fe21cdb1778748d8fbefe61cc04d1ed96771
indrish38 <indrishsundar@gmail.com>
2023-11-20 00:14:38 +0530

    update

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 api_id = '23275262'
 api_hash = '13d854f2fa5a9ba3c03639fd67c522aa'
-bot_token = '6533996464:AAF9JinShEREKWnwwZyD396fOmLnxmN-j0A'
+bot_token = '6897981040:AAFgEzcBoT2b1lqMd_b9ce3QepJE9XNQ2pc'
+# bot_token = '6533996464:AAF9JinShEREKWnwwZyD396fOmLnxmN-j0A'
