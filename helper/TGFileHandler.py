98f1c308689f9f64412eeb048b3ee4ac549db60f
indrish38 <indrishsundar@gmail.com>
2023-11-19 18:33:34 +0530

    update

--- helper/TGFileHandler.py
+++ helper/TGFileHandler.py
@@ -13,16 +13,6 @@ class TGFileHandler:
         path = message.command[1]
         file_name = os.path.basename(path)
         msg = message.reply('Uploading...', True)
-        async def progress(current, total):
-            # print(current,total)
-            val = current * 30 // total
-            txt = f'''Uploading...\n[{val*'▣'}{(30-val)*'▢'}] {current*100/total:.2f}%
-            File Name : {file_name}
-            Progress : {current/1024/1024:.2f} of {total/1024/1024:.2f} MB'''
-            if(msg.text != txt):
-                await msg.edit_text(txt)
-                msg.text = txt
-            t.sleep(8)
         file = open(path, 'rb')
         size = (os.path.getsize(path))
         if(size > 2147483648):
@@ -30,23 +20,14 @@ class TGFileHandler:
             subprocess.run(f"7z -mx0 a -v2047M {path[:path.rfind('/')]}/cache/{file_name} {path}".split(" "))
             for f in os.listdir(f"{path[:path.rfind('/')]}/cache"):
                 # print(f)
-                message.reply_document(open(f"{path[:path.rfind('/')]}/cache/{f}", 'rb'), True, progress=progress, file_name = f)
+                message.reply_document(open(f"{path[:path.rfind('/')]}/cache/{f}", 'rb'), True, progress=progress, progress_args = 'Up', file_name = f)
         else:
-            message.reply_document(file, True, caption=file_name, progress=progress, file_name = file_name)
+            message.reply_document(file, True, caption=file_name, progress=progress, progress_args = 'Up', file_name = file_name)
         msg.delete()
 
     def download_media(client, message):
         print(message.text)
-        async def progress(current, total, type):
-            print(current,total)
-            val = current * 30 // total
-            txt = f'''{type}loading...\n[{val*'█'}{(30-val)*'▒'}] {current*100/total:.2f}%\n
-            File Name : {message.document.file_name if message.document is not None else message.video.file_name}
-            Progress : {current/1024/1024:.2f} of {total/1024/1024:.2f} MB'''
-            if(msg.text != txt):
-                await msg.edit_text(txt)
-                msg.text = txt
-            t.sleep(5)
+        
         msg = message.reply('Downloading...', True)
         while message.reply_to_message and message.reply_to_message.media:
             # print(message)
@@ -65,3 +46,25 @@ class TGFileHandler:
             # print(data)
             urllib.request.urlretrieve(message.command[1], filename=f'downloads/{os.path.basename(data.path)}')
         message.reply_text("Download Completed.", True)
+
+    async def progress(current, total, type):
+        print(current,total)
+        val = current * 30 // total
+        txt = f'{type}loading...\n[{val*'▣'}{(30-val)*'▢'}] {current*100/total:.2f}%\n' + \
+        'File Name : {message.document.file_name if message.document is not None else message.video.file_name}\n' + \
+        'Progress : {current/1024/1024:.2f} of {total/1024/1024:.2f} MB'
+        if(msg.text != txt):
+            await msg.edit_text(txt)
+            msg.text = txt
+        t.sleep(5)
+
+        # async def progress(current, total):
+        #     # print(current,total)
+        #     val = current * 30 // total
+        #     txt = f'''Uploading...\n[{val*'▣'}{(30-val)*'▢'}] {current*100/total:.2f}%
+        #     File Name : {file_name}
+        #     Progress : {current/1024/1024:.2f} of {total/1024/1024:.2f} MB'''
+        #     if(msg.text != txt):
+        #         await msg.edit_text(txt)
+        #         msg.text = txt
+        #     t.sleep(8)
