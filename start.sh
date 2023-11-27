ae52dfccc3f1537d48b1785e0dd9df391aa74607
indrish38 <indrishsundar@gmail.com>
2023-11-27 21:37:49 +0530

    update

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@ wget -q https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip -qqj rclone-current-linux-amd64.zip
 rm -rf rclone-current-linux-amd64.zip
 export PATH=$PWD/:$PATH
-# wget https://gist.githubusercontent.com/itsJack038/e8019d9ec07e3baed9fbd8bf43970035/raw/rclone.conf -O rclone.conf
-# cp rclone.conf /app/.config/rclone/rclone.conf
+wget https://gist.githubusercontent.com/itsJack038/e8019d9ec07e3baed9fbd8bf43970035/raw/rclone.conf -O /app/.config/rclone/rclone.conf
 python bot.py
