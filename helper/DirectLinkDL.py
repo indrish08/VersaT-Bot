977550459d2d6953fe1e1c2ece41ec2e9a8316a1
indrish38 <indrishsundar@gmail.com>
2023-11-27 21:41:51 +0530

    update

--- helper/DirectLinkDL.py
+++ helper/DirectLinkDL.py
@@ -5,7 +5,7 @@ import urllib.request
 class DirectLinkDL:
     def download(message):
         msg = message.reply('Downloading...', True)
-        os.system(f'wget "{message.command[1]}" -P ./downloads')
+        os.system(f'wget -q "{message.command[1]}" -P ./downloads')
         msg.edit_text(f"Downloaded successfully to: \n`'downloads'`")
         return os.path.basename('https://mirror.jackdrive038.workers.dev/0:/Avengers.Infinity.War.2018.2160p.10bit.HDR.BluRay.8CH.x265.HEVC_PSA.mkv')
         # data = urllib.parse.urlparse(message.command[1])
