4862a008987c9f58187fe5fcf7e39e492a00cb39
indrish38 <indrishsundar@gmail.com>
2023-12-02 20:40:49 +0530

    Reply-chain implemented for Upload.

--- helper/TGFileHandler.py
+++ helper/TGFileHandler.py
@@ -22,28 +22,10 @@ async def progress(current, total, type, msg, start, file_name):
             f"**Progress :** {size_h(current)} of {size_h(total)}"
         await msg.edit_text(txt)
 
-def upload(client, message):
-    print(message.text)
-    if (len(message.command) == 1):
-        message.reply('Give link or file path to Upload.', True)
-        return
-    if message.command[1].startswith('https://alchemist.cyou/'):
-        path = RC.download(message)
-    elif message.command[1].startswith('https://drive.google.com/'):
-        path = GDL.download(message.command[1])
-    elif message.command[1].startswith('http'):
-        path = DDL.download(message)
-    else:
-        path = message.text[message.text.find(' ')+1 : ]
-
-    if os.path.isdir(path):
-        upload_folder(path, message)
-    else:
-        upload_media(path, message)
-
-def upload_media(path, message):
+def upload_media(path, message, reply_to = None):
     size = os.path.getsize(path)
     file_name = os.path.basename(path)
+    reply_to = message.id if reply_to == None else reply_to
 
     if (size > split_size):
         msg = message.reply('Splitting...', True)
@@ -53,14 +35,47 @@ def upload_media(path, message):
         os.system(f'rar -m0 a -v{split_size}B \"{os.path.join(new_path, file_name)}\" \"{path}\"')
         files = sorted(os.listdir(new_path))
         for file in files:
-            message.reply_document(os.path.join(new_path, file), True, caption=f'`{file}`', progress=progress, 
-                                    progress_args=['Up', msg, t.time(), file], file_name=file)
+            reply = message.reply_document(os.path.join(new_path, file), caption=f'`{file}`', progress=progress, 
+                                    progress_args=['Up', msg, t.time(), file], file_name=file, reply_to_message_id=reply_to)
         shutil.rmtree(new_path)
     else:
         msg = message.reply('Uploading...', True)
-        message.reply_document(path, True, caption=f'`{file_name}`', progress=progress, 
-                                progress_args=['Up', msg, t.time(), file_name], file_name=file_name)
+        reply = message.reply_document(path, caption=f'`{file_name}`', progress=progress, 
+                                progress_args=['Up', msg, t.time(), file_name], file_name=file_name, reply_to_message_id=reply_to)
     msg.delete()
+    return reply
+
+def upload_folder(path, message, reply_to = None):
+    reply_to = message.id if reply_to == None else reply_to
+    reply = message.reply(f"**{os.path.basename(path)}**", reply_to_message_id=reply_to)
+    reply_f = reply
+    cur_dir = sorted(os.listdir(path))
+    for folder in cur_dir:
+        folder = os.path.join(path,folder)
+        if(os.path.isdir(folder)):
+            upload_folder(folder, message, reply.id)
+    for file in cur_dir:
+        file = os.path.join(path,file)
+        if(os.path.isfile(file)):
+            reply = upload_media(file, message, reply.id)
+            t.sleep(1)
+    # message.reply(f"**{'*'*11}**", reply_to_message_id=reply_f.id)
+
+def upload(client, message):
+    print(message.text)
+    if (len(message.command) == 1):
+        message.reply('Give link or file path to Upload.', True)
+        return
+    if message.command[1].startswith('http'):
+        path = download_media(message)
+    else:
+        path = message.text[message.text.find(' ')+1 : ]
+
+    if os.path.isdir(path):
+        upload_folder(path, message)
+    else:
+        upload_media(path, message)
+    message.reply(f"Upload Completed : {os.path.basename(path)}")
 
 def download_media(client, message, progress=progress):
     print(message.text)
@@ -74,26 +89,15 @@ def download_media(client, message, progress=progress):
         else:
             message.reply_text("Please tag a media message with the /download command.", True)
         return
-    elif message.command[1].startswith('https://alchemist.cyou/'):
+    if message.command[1].startswith('https://alchemist.cyou/'):
         file_path = RC.download(message)
     elif message.command[1].startswith('https://drive.google.com/'):
         file_path = GDL.download(message.command[1])
     elif message.command[1].startswith('http'):
-        file_path = DDL.download(message, msg)
+        file_path = DDL.download(message)
     
     if file_path is None:
-        msg.edit_text('Downloaded Failed!')
+        message.reply_text('Downloaded Failed!',True)
     else:
-        msg.edit_text("Downloaded successfully to: \n`{}`".format(file_path.replace('\\', '/')))
-
-def upload_folder(path, message):
-    message.reply(f"**Folder : {os.path.basename(path)}**",True)
-    for file in sorted(os.listdir(path)):
-        file = os.path.join(path,file)
-        if(os.path.isdir(file)):
-            upload_folder(file, message)
-        else:
-            upload_media(file, message)
-            t.sleep(1)
-    message.reply(f"**Folder '{os.path.basename(path)}' Completed.**",True)
-    
+        message.reply_text("Downloaded successfully to: \n`{}`".format(file_path.replace('\\', '/')), True)
+    msg.delete()
